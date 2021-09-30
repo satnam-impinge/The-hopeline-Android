275e8a598d7ecc40cce1ac7cdefd63f5f8186a55
satnam <stanam.impinge@gmail.com>
2021-09-30 17:46:46 +0530

    bug fixes and intercom integration

--- app/src/main/java/com/payoda/hopeline/MyApplication.java
+++ app/src/main/java/com/payoda/hopeline/MyApplication.java
@@ -2,10 +2,15 @@ package com.payoda.hopeline;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 
 
 import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
+
+import io.intercom.android.sdk.Intercom;
+import io.intercom.android.sdk.UserAttributes;
 
 
 public class MyApplication extends MultiDexApplication {
@@ -25,6 +30,9 @@ public class MyApplication extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         mInstance = this;
+        new GetPublicIP().execute();
+        Intercom.initialize(this, "android_sdk-597c68cec8b16403fc727d3f31acaba99f8c7abe", "pnsu3l0n");
+        Intercom.client().registerUnidentifiedUser();
 
 
     }
@@ -33,5 +41,40 @@ public class MyApplication extends MultiDexApplication {
         return mInstance;
     }
 
+    public class GetPublicIP extends AsyncTask<String, String, String> {
 
+        @Override
+        protected String doInBackground(String... strings) {
+            String publicIP = "";
+            try {
+                java.util.Scanner s = new java.util.Scanner(
+                        new java.net.URL(
+                                "https://api.ipify.org")
+                                .openStream(), "UTF-8")
+                        .useDelimiter("\\A");
+                publicIP = s.next();
+                System.out.println("My current IP address is " + publicIP);
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+
+            return publicIP;
+        }
+
+        @Override
+        protected void onPostExecute(String publicIp) {
+            super.onPostExecute(publicIp);
+            String IP_ADDRESS = publicIp;
+            Log.e("ips_address", IP_ADDRESS);
+
+            UserAttributes userAttributes = new UserAttributes.Builder()
+                    .withCustomAttribute("chatPartner", "TheHopeLine Android App")
+                    .withCustomAttribute("ipaddress", IP_ADDRESS)
+                    .build();
+            Intercom.client().updateUser(userAttributes);
+
+        }
+
+
+    }
 }
--- app/src/main/java/com/payoda/hopeline/help/ActivityGetHelp.java
+++ app/src/main/java/com/payoda/hopeline/help/ActivityGetHelp.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
+
 import com.livechatinc.inappchat.ChatWindowActivity;
 import com.payoda.hopeline.MyApplication;
 import com.payoda.hopeline.R;
@@ -16,10 +17,11 @@ import com.payoda.hopeline.main.ActivityWebView;
 import com.payoda.hopeline.utils.AppUtils;
 import com.payoda.hopeline.utils.GlobalConsts;
 
+import io.intercom.android.sdk.Intercom;
 
-public class ActivityGetHelp extends Activity implements View.OnClickListener
-{
-    private Context context=this;
+
+public class ActivityGetHelp extends Activity implements View.OnClickListener {
+    private Context context = this;
     TextView footer;
 
     @Override
@@ -29,20 +31,22 @@ public class ActivityGetHelp extends Activity implements View.OnClickListener
         setContentView(R.layout.activity_gethelp);
 
         AppUtils.setScreenName(this, "Get Help");
+        initChatButton();
+
 
         GlobalConsts.initBack(this);
 
         findViewById(R.id.chat_with_us).setOnClickListener(this);
         findViewById(R.id.stories).setOnClickListener(this);
-       /* findViewById(R.id.call_us).setOnClickListener(this);*/
+        /* findViewById(R.id.call_us).setOnClickListener(this);*/
         findViewById(R.id.blog).setOnClickListener(this);
         findViewById(R.id.request).setOnClickListener(this);
         findViewById(R.id.eBooks).setOnClickListener(this);
         findViewById(R.id.follow).setOnClickListener(this);
 
-        ((TextView)findViewById(R.id.head).findViewById(R.id.heading)).setText("Get Help");
+        ((TextView) findViewById(R.id.head).findViewById(R.id.heading)).setText("Get Help");
 
-        footer=(TextView)findViewById(R.id.bottomMark).findViewById(R.id.markText);
+        footer = (TextView) findViewById(R.id.bottomMark).findViewById(R.id.markText);
         if (GlobalConsts.footerLink_model.getMobile_app_marquee().length() < 80) {
             String temp = " ";
             for (int i = 0; i < (80 - GlobalConsts.footerLink_model.getMobile_app_marquee().length()); i++)
@@ -66,7 +70,6 @@ public class ActivityGetHelp extends Activity implements View.OnClickListener
         });
 
 
-
 //        WebView view1 = (WebView) findViewById(R.id.webView_footer);
 //        view1.getSettings().setLoadsImagesAutomatically(true);
 //        view1.getSettings().setJavaScriptEnabled(true);
@@ -74,6 +77,12 @@ public class ActivityGetHelp extends Activity implements View.OnClickListener
 //        view1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 //        view1.loadUrl(GlobalConsts.bete_URL + "mobile-app-marquee/");
 
+    }
+
+    private void initChatButton() {
+        Intercom.client().setLauncherVisibility(Intercom.Visibility.VISIBLE);
+        Intercom.client().setBottomPadding(10);
+        Intercom.client().getUnreadConversationCount();
     }
 
     @SuppressWarnings("deprecation")
@@ -85,9 +94,9 @@ public class ActivityGetHelp extends Activity implements View.OnClickListener
             @Override
             public void run() {
                 Intent intent = null;
-                switch (v.getId())
-                {
+                switch (v.getId()) {
                     case R.id.chat_with_us:
+                        Intercom.client().displayMessenger();
 
 //                        intent = new Intent(context, ActivityWebView.class);
 //                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL+"mobile-signup");
@@ -95,19 +104,19 @@ public class ActivityGetHelp extends Activity implements View.OnClickListener
 //                        startActivity(intent);
 
 
-                        intent = new Intent(context,ChatWindowActivity.class);
+                        /*intent = new Intent(context,ChatWindowActivity.class);
                         intent.putExtra(ChatWindowActivity.KEY_GROUP_ID, "16");
                         intent.putExtra(ChatWindowActivity.KEY_LICENCE_NUMBER, "6982601");
 //                        intent.putExtra(ChatWindowActivity.KEY_VISITOR_NAME, "Client");
 //                        intent.putExtra(ChatWindowActivity.KEY_VISITOR_EMAIL, "client@example.com");
-                        startActivity(intent);
+                        startActivity(intent);*/
                         v.setBackgroundColor(getResources().getColor(R.color.green_demanded));
 
                         break;
 
                     case R.id.stories:
                         intent = new Intent(context, ActivityWebView.class);
-                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL+"storiespage/");
+                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL + "storiespage/");
                         intent.putExtra(GlobalConsts.EXTRA_TITLE, "Stories");
                         startActivity(intent);
                         v.setBackgroundColor(getResources().getColor(R.color.stories));
@@ -120,7 +129,7 @@ public class ActivityGetHelp extends Activity implements View.OnClickListener
 //                        v.setBackgroundColor(getResources().getColor(R.color.stories));
 
 //                        intent = new Intent(context, InstagramFeed.class);
-                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL+"get-twitter-feeds-mobile-app/");
+                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL + "get-twitter-feeds-mobile-app/");
                         intent.putExtra(GlobalConsts.EXTRA_TITLE, "Twitter Feed");
                         startActivity(intent);
                         v.setBackgroundColor(getResources().getColor(R.color.twitter_color));
@@ -136,7 +145,7 @@ public class ActivityGetHelp extends Activity implements View.OnClickListener
 
                     case R.id.blog:
                         intent = new Intent(context, ActivityWebView.class);
-                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL+"tag-list-mobile/");
+                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL + "tag-list-mobile/");
                         intent.putExtra(GlobalConsts.EXTRA_TITLE, "Topics");
                         startActivity(intent);
                         v.setBackgroundColor(getResources().getColor(R.color.blog_color));
@@ -150,7 +159,7 @@ public class ActivityGetHelp extends Activity implements View.OnClickListener
 
                     case R.id.eBooks:
                         intent = new Intent(context, ActivityWebView.class);
-                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL+"ebooks-for-iphone-ipad-android/");
+                        intent.putExtra(GlobalConsts.EXTRA_URL, GlobalConsts.URL + "ebooks-for-iphone-ipad-android/");
                         intent.putExtra(GlobalConsts.EXTRA_TITLE, "eBooks");
                         startActivity(intent);
                         v.setBackgroundColor(getResources().getColor(R.color.ebooks_color));
